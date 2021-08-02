5d3ec263072b1d5681acc49bfb415ee29b13546e
waranyu26 <waranyu.mm26@gmail.com>
2021-08-02 07:13:52 +0700

    remove unused comment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers, network } from "hardhat";
 import {
   CONFIRMATION,
@@ -17,20 +12,6 @@ import { isLocalNetwork, deployExternalContracts, verify } from "./utils";
 const { provider } = ethers;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  // const Greeter = await ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  // await greeter.deployed();
-
-  // console.log("Greeter deployed to:", greeter.address);
   const [owner] = await ethers.getSigners();
   console.log("DEPLOYMENT START...");
 
@@ -63,8 +44,6 @@ async function deployAutoFarmer(owner: SignerWithAddress): Promise<Contract> {
   return farmer;
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
